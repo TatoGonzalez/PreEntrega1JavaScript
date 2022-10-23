d0500cbb37e8444c5c40736cc3aacc16c00c95b0
Cristian Gonzalez <desarrolloweb.cgonzalez@gmail.com>
2022-10-23 16:44:36 -0300

    Primer Paso

---
+++ main.js
@@ -0,0 +1,10 @@
+let edad = prompt("Ingrese su edad");
+
+if (edad >= 18) {
+    alert("Sos mayor de edad. Presiona Solicitar tu Presupuesto para continuar");
+}
+else { 
+    alert("Sos menor de edad. Lamentablemente solo podemos ofrecer presupuestos a mayores de edad")
+}
+
+
---
+++ main2.js
@@ -0,0 +1,45 @@
+
+let precio = 2000;
+let presupuestoFinal = true;
+
+let nombre = prompt("Bienvenid@, ingrese su nombre por favor");
+
+let personas = Number(prompt("Ingrese en numeros la cantidad de personas/invitados al evento"));
+
+if (personas > 0) {
+    presupuestoFinal = true;
+}
+else {
+    alert("No ingreso un numero valido.")
+}
+
+
+let costo = (precio*personas);
+let casamiento = 1.5;
+let cumpleanios = 1.5;
+let empresarial = 2;
+let otro = 2;
+
+
+let tipoEvento = Number(prompt( "Elija el tipo de evento a realizar - Ingrese numero que corresponda \n1- Casamiento \n2- Cumpleaños \n3- Empresarial \n4- Otro"));
+
+
+while (presupuestoFinal = true) {
+    switch (tipoEvento) {
+        case 1:
+            alert("El presupuesto aproximado para tu evento es " + "$" + (costo * casamiento) + ". Gracias por elegirnos " + nombre + "! " + " Contactate a nuestro WhatsApp o mediante nuestras Redes Sociales para recibir atencion personalizada!" );
+            break;
+        case 2:
+            alert("El presupuesto aproximado para tu evento es " + "$" +(costo * cumpleanios) + ". Gracias por elegirnos " + nombre + "! "  + " Contactate a nuestro WhatsApp o mediante nuestras Redes Sociales para recibir atencion personalizada!");
+            break;
+        case 3:
+            alert("El presupuesto aproximado para tu evento es " + "$" + (costo * empresarial) + ". Gracias por elegirnos " + nombre + "! " + " Contactate a nuestro WhatsApp o mediante nuestras Redes Sociales para recibir atencion personalizada!" );
+            break;
+        case 4:
+            alert("El presupuesto aproximado para tu evento es " + "$" + (costo * otro) + ". Gracias por elegirnos " + nombre + "! " + " Contactate a nuestro WhatsApp o mediante nuestras Redes Sociales para recibir atencion personalizada!");
+            break;
+    };
+};
+
+
+alert("Contactate a nuestro WhatsApp o mediante nuestras Redes Sociales para recibir atencion personalizada!")
